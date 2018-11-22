f6e3f1fc44beabce4ed555530e6845a243cab8f8
Huong Dang <huongd97@knights.ucf.edu>
2018-11-21 23:32:36 -0500

    Sent email via mailgun api successfully\!

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import Button from '@material-ui/core/Button';
+import axios from 'axios';
+import { useState } from 'react';
+
 const Index = () => {
-    return <div>Hello! {process.env.HELLO_WORLD}</div>;
+    const [error, setError] = useState('');
+    const sendTestEmail = async () => {
+        try {
+            const res = await axios.post('/sendHTMLEmail');
+            setError('res', JSON.stringify(res));
+            console.log('res.data', res.data);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
+    return (
+        <div>
+            Hello!
+            <p>Press button to send a test email</p>
+            <Button onClick={sendTestEmail}>Test!</Button>
+            <p>{error}</p>
+        </div>
+    );
 };
 
 export default Index;
--- server.js
+++ server.js
@@ -5,10 +5,55 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+const api_key = escape(process.env.MAILGUN_API_KEY);
+const DOMAIN = 'sandbox6580114275734466bcb6d7f5550776fb.mailgun.org';
+const mailgun = require('mailgun-js')({
+    apiKey: api_key,
+    domain: DOMAIN,
+});
+// const testEmailAddress = `huongd97@gmail.com`;
+const testEmailAddresses = [
+    'huongsocial@gmail.com',
+    'huongd97@gmail.com',
+    'jannyjenny811@gmail.com',
+    'huong.cfp@gmail.com',
+];
+const testEmailAddress = `huongsocial@gmail.com`;
+const myAccount = `Huong Dang <huongd97@gmail.com>`;
+
 app.prepare()
     .then(() => {
         const server = express();
 
+        server.post('/sendHTMLEmail', (req, res) => {
+            const payload = {
+                from: myAccount,
+                to: testEmailAddress,
+                subject: `An HTML email`,
+                text: 'Sending message from me to me :)',
+                html: `
+                <html>
+                <head></head>
+                <body><p><b>what up</b></p></body>
+                </html>
+                `,
+            };
+        });
+        server.post('/sendEmail', (req, res) => {
+            const data = {
+                from: 'Huong Dang <huongd97@gmail.com>',
+                to: testEmailAddress,
+                subject: 'Hello',
+                text: 'Sending message from me to me :)',
+            };
+
+            mailgun.messages().send(data, function(error, body) {
+                if (error) res.json(error);
+                else {
+                    res.json(body);
+                }
+            });
+        });
         server.get('*', (req, res) => {
             return handle(req, res);
         });
